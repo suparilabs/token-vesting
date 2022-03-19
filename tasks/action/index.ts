d9cea21b902b3d34f2161180b7f6649a06c3dd49
dhruvinparikh <imsilent93@gmail.com>
2022-03-19 17:30:28 -0400

    ci(tasks): transfer ownership

--- tasks/action/index.ts
+++ tasks/action/index.ts
@@ -1,3 +1,8 @@
 import "./createvestingschedule";
 import "./fundTokenVesting";
 import "./timestamp";
+import "./ido-transfer-ownership";
+import "./private-pretimelock-transfer-ownership";
+import "./private-prevesting-transfer-ownership";
+import "./seed-pretimelock-transfer-ownership";
+import "./seed-prevesting-transfer-ownership";
